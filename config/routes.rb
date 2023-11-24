68463d9df8efe5b16ac6d1c49fde46babe3f31ab
= <=>
2023-11-24 13:06:54 +0100

    Naprawiono błędy po tym kretynie :)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :films
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +9,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root "home#index"
+  get "home/about"
+  get "home/contact"
 end
