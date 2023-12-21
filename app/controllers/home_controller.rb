db5c389888e91c450482fd7d1b422e681d7050ab
Zbigniew Przybyła <zprzybyla@tuta.io>
2023-12-21 14:44:39 +0100

    Pytanie o cookies

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,5 +7,7 @@ class HomeController < ApplicationController
         Client.create(:browser => cc.name, :os => cc.os_name, :dev_type => cc.device_type)
 
         @clients = Client.all
+
+        session[:cookies_accepted] = params[:cookies] if params[:cookies]
     end
 end
