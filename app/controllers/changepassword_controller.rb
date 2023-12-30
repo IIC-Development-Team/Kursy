1ccd4787ec44ba9dce2aef106db6689e33d85481
suwalek <jakub.suwala@interia.pl>
2023-12-30 14:49:25 +0100

    Update Forgot Password Site

---
+++ app/controllers/changepassword_controller.rb
@@ -0,0 +1,2 @@
+class ChangepasswordController < ApplicationController
+end
---
+++ test/controllers/changepassword_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ChangepasswordControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
